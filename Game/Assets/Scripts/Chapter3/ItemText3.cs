ca12f372e7c4b34994d179ebde42d1c9a7cee659
hanby <hanby.choi@ewhain.net>
2022-01-13 23:20:58 +0900

    아이템 닿았을 때 효과음 추가
    ItemText2/3 적용
    집사/고양이 텍스트 전환 및 효과음 재생 기능 추가
    개별 스테이지에서는 정상작동하나 다음 스테이지로 넘어가는 경우 2개의 null reference 오류가 발생함
    씬 전환과 동시에 꺼지는 경우: NullReferenceException: Object reference not set to an instance of an object
    CatControlAndSound.OnCollisionEnter2D (UnityEngine.Collision2D collision) (at Assets/Scripts/Chapter1/CatControlAndSound.cs:83)
    씬 전환 후 아이템에 닿았을 때 꺼지는 경우:
    NullReferenceException: Object reference not set to an instance of an object
    ItemText3.OnTriggerEnter2D (UnityEngine.Collider2D collision) (at Assets/Scripts/Chapter3/ItemText3.cs:32)

--- Game/Assets/Scripts/Chapter3/ItemText3.cs
+++ Game/Assets/Scripts/Chapter3/ItemText3.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// 아이템 근처로 가면 메시지가 나오게 하는 스크립트
 public class ItemText3 : MonoBehaviour
 {
+    [SerializeField]
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioClip clip;
+
     private Transform text;
 
     // Start is called before the first frame update
     void Start()
     {
-        text = gameObject.transform.GetChild(0);
+        if (Management.staff == 0)
+            text = gameObject.transform.GetChild(0);
+        else if (Management.staff == 3)
+            text = gameObject.transform.GetChild(1);
     }
 
     // Update is called once per frame
@@ -22,16 +30,20 @@ public class ItemText3 : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         text.gameObject.SetActive(true);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
     }
 
     // 맵 크기에 따라 collider 크기와 거리 조정만 하면 됩니다.
     private void OnTriggerStay2D(Collider2D collision)
     {
-        //Debug.Log(gameObject.transform.position.x - collision.transform.position.x);
         if (gameObject.transform.position.x - collision.transform.position.x > -1
             && gameObject.transform.position.x - collision.transform.position.x < 1)
         {
-            text.GetComponent<TextMesh>().color = new Color32(255, 255, 255, 255); //white
+            text.GetComponent<TextMesh>().color = new Color32(255, 255, 255, 255);
         }
         else
         {
@@ -42,5 +54,6 @@ public class ItemText3 : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         text.gameObject.SetActive(false);
+        audioSource.Stop();
     }
 }
